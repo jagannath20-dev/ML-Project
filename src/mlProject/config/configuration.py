7cd471450e35d62ee42fa22bccdb97820b934659
jagannath20-dev <devarakondajagannath2580@yahoo.com>
2024-01-09 13:03:53 +0530

    Data Validaton Completed

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import (DataIngestionConfig)
+from mlProject.entity.config_entity import (DataIngestionConfig,
+                                            DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -15,14 +16,34 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
         create_directories([config.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
-            root_dir = config.root_dir, 
-            source_URL = config.source_URL,
-            local_data_file = config.local_data_file,
-            unzip_dir = config.unzip_dir)
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir 
+        )
+
         return data_ingestion_config
+
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            STATUS_FILE = config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema = schema
+        )
+        return data_validation_config
+
+        
